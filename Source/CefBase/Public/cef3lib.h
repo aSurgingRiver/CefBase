3fdc0bb9b8a17d9045f7a99e3482ae98957af31c
asurgingriver <aSurgingRiver@163.com>
2024-06-23 17:15:47 +0800

    for linux

---
+++ Source/CefBase/Public/cef3lib.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Interfaces/IPluginManager.h"
+#include <vector>
+
+// WEB_CORE_API
+class CEFBASE_API ICEF3LIB {
+public:
+	static ICEF3LIB* get();
+	virtual void LoadCEF3Modules() = 0;
+	virtual void UnloadCEF3Modules() = 0;
+	virtual FString LibPath() = 0;
+	virtual int Branch() = 0;
+};
+
+
